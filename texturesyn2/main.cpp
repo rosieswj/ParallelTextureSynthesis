77eae5c77c23f40c8ff4316b85695746ab867849
Rosie Sun <weijias1@andrew.cmu.edu>
2020-04-20 18:39:36 -0400

    adding command line arg

--- texturesyn2/main.cpp
+++ texturesyn2/main.cpp
@@ -1,21 +1,79 @@
 #include "texture.h"
 #include "lib/cycletimer.h"
 #include "lib/instrument.h"
+#include <getopt.h>
 
-int main()
+static void usage(char *name)
+{
+    const char *use_string = "-s SFILE -o OFILE -w WINDOWS -r RADIUS [-I]";
+    outmsg("Usage: %s %s\n", name, use_string);
+    outmsg("   -h         Print this message\n");
+    outmsg("   -s SFILE   Sample file\n");
+    outmsg("   -o OFILE   Output file\n");
+    outmsg("   -w WINDOW  Window size\n");
+    outmsg("   -r RADIUS  Output radius\n");
+    outmsg("   -I         Instrument simulation activities\n");
+    exit(0);
+}
+
+int main(int argc, char *argv[])
 {
     //user defined parameters
-    int WINDOW = 10;
-    int RADIUS = 100;
-    const string INPUT = "./src/sample1.ttr";
-    const string OUTPUT = "./out/sample1-out-";
+    int WINDOW;
+    int RADIUS;
 
-    bool instrument = true;
+    char *INPUT = NULL;
+    char *OUTPUT = NULL;
+
+    bool instrument = false;
+    int c;
+
+    const char *optstring = "hs:o:w:r:I";
+    while ((c = getopt(argc, argv, optstring)) != -1)
+    {
+        switch (c)
+        {
+        case 'h':
+            usage(argv[0]);
+            break;
+        case 's':
+            INPUT = optarg;
+            break;
+        case 'o':
+            OUTPUT = optarg;
+            break;
+        case 'w':
+            WINDOW = atoi(optarg);
+            break;
+        case 'r':
+            RADIUS = atoi(optarg);
+            break;
+        case 'I':
+            instrument = true;
+            break;
+        default:
+            outmsg("Unknown option '%c'\n", c);
+            usage(argv[0]);
+            exit(1);
+        }
+    }
+    if (INPUT == NULL)
+    {
+        outmsg("Please give sample path\n");
+        usage(argv[0]);
+    }
+    if (OUTPUT == NULL)
+    {
+        outmsg("Please give output path\n");
+        usage(argv[0]);
+    }
+    printf("w=%d, r=%d\n", WINDOW, RADIUS);
+
     track_activity(instrument);
-
     //read sample image
     START_ACTIVITY(ACVIVITY_IMAGE);
     Image sampleImg(INPUT);
+
     int sw = sampleImg.width;
     int sh = sampleImg.height;
     double **sample = getRGB(sampleImg);
@@ -25,13 +83,13 @@ int main()
     {
         res[i] = new double[3]; //RGB value
     }
-    printf("\n############## read sample done ##################\n");
+    // printf("\n############## read sample done ##################\n");
     printf("[sample] %d x %d, size = %d\n", sw, sh, sw * sh);
     printf("[result] %d x %d, size = %d\n", rsize, rsize, rsize * rsize);
     FINISH_ACTIVITY(ACVIVITY_IMAGE);
 
     synthesize(sample, res, RADIUS, WINDOW, sw, sh);
-    printf("\n############## synthesize done #####################\n");
+    // printf("\n############## synthesize done #####################\n");
 
     //write result image
     START_ACTIVITY(ACVIVITY_IMAGE);
@@ -40,6 +98,8 @@ int main()
 
     deleteRGB(res, rsize, rsize);
     deleteRGB(sample, sw, sh);
+
     SHOW_ACTIVITY(stderr, instrument);
+    printf("DONE\n");
     return 0;
 }
--- texturesyn2/textureutil.cpp
+++ texturesyn2/textureutil.cpp
@@ -87,3 +87,14 @@ string int2str(int x)
     stream << x;
     return stream.str();
 }
+
+void outmsg(const char *fmt, ...)
+{
+    va_list ap;
+    bool got_newline = fmt[strlen(fmt) - 1] == '\n';
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    if (!got_newline)
+        fprintf(stderr, "\n");
+}
